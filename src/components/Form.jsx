9803ffe2f739a48c0cf1f58ee29e67f928a209da
alamin-shanto <mdalaminshanto1993@gmail.com>
2024-06-14 08:49:36 +0600

    new code added

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,12 +1,28 @@
+import { useState } from "react";
+
 export default function Form() {
-  const [name, setName]= useState {""};
-  function handleChange() {
-    console.log("Changed Occured");
-  }
+  const [name, setName] = useState({ firstName: "", lastName: "" });
+  // function handleChange(e) {
+  //   setName(e.target.value);
+  // }
   return (
     <div>
+      {name.firstName} - {name.lastName}
       <form>
-        <input onChange={handleChange} type="text" value="name"></input>
+        <input
+          // onChange={function demo(e) {
+          //   return handleChange(e);
+          // }}
+          // onChange={(e) => handleChange(e)}
+          onChange={(e) => setName({ ...name, firstName: e.target.value })}
+          type="text"
+          value={name.firstName}
+        ></input>
+        <input
+          onChange={(e) => setName({ ...name, lastName: e.target.value })}
+          type="text"
+          value={name.lastName}
+        ></input>
       </form>
     </div>
   );
